607dfd5cb296283029e68dd6f87cdd9f109b1931
Angel Faryshta Guevara <angeldelcaos@gmail.com>
2017-02-08 16:45:44 -0600

    delete Helper\Api

--- backend/config/main.php
+++ backend/config/main.php
@@ -46,7 +46,7 @@ return [
             'csrfParam' => '_csrf-backend',
         ],
         'user' => [
-            'identityClass' => 'backend\api\models\User',
+            'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
             'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
         ],
