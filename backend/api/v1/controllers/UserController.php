d285b6a7d619aa90df22830ad897706f1d1e33c7
Angel Faryshta Guevara <angeldelcaos@gmail.com>
2017-02-09 18:41:43 -0600

    simplify controllers by using ResourceController

--- backend/api/v1/controllers/UserController.php
+++ backend/api/v1/controllers/UserController.php
@@ -3,20 +3,9 @@
 namespace backend\api\v1\controllers;
 
 use backend\api\models\User;
-use Yii;
-use api\actions\CompartirAction;
-use filsh\yii2\oauth2server\filters\ErrorToExceptionFilter;
-use filsh\yii2\oauth2server\filters\auth\CompositeAuth;
-use yii\data\ActiveDataProvider;
-use yii\db\ActiveQuery;
-use yii\db\ActiveRecord;
-use yii\filters\auth\HttpBearerAuth;
-use yii\filters\auth\QueryParamAuth;
-use yii\helpers\ArrayHelper;
-use yii\rest\ActiveController;
-use yii\web\NotFoundHttpException;
+use tecnocen\roa\controllers\OAuth2ResourceController;
 
-class UserController extends ActiveController
+class UserController extends OAuth2ResourceController
 {
     /**
      * @inheritdoc
@@ -26,32 +15,10 @@ class UserController extends ActiveController
     /**
      * @inheritdoc
      */
-    public function behaviors()
+    public function verbs()
     {
-        return ArrayHelper::merge(parent::behaviors(), [
-            'authenticator' => [
-                'class' => CompositeAuth::class,
-                'authMethods' => [
-                    ['class' => HttpBearerAuth::class],
-                    [
-                        'class' => QueryParamAuth::class,
-                        // Importante, parametro get a usar.
-                        'tokenParam' => 'accessToken',
-                    ],
-                ],
-            ],
-            'exceptionFilter' => ['class' => ErrorToExceptionFilter::class],
-        ]);
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function actions()
-    {
-        return array_merge(parent::actions(), [
-            'create' => null,
-            'delete' => null,
-        ]);
+        $verbs = parent::verbs();
+        unset($verbs['delete'], $verbs['create']);
+        return $verbs;
     }
 }
