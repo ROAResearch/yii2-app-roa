8f4ab45698f90a19805747d9f84cee3d621a00dc
Angel (Faryshta) Guevara <angeldelcaos@gmail.com>
2018-07-25 12:38:11 -0500

    separate codeception and yii_test configuration files

---
+++ environments/dev/common/config/codeception-local.php
@@ -0,0 +1,13 @@
+<?php
+
+return yii\helpers\ArrayHelper::merge(
+    require __DIR__ . '/test-local.php',
+    [
+        'components' => [
+            'request' => [
+                // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+                'cookieValidationKey' => '',
+            ],
+        ],
+    ]
+);
--- environments/index.php
+++ environments/index.php
@@ -44,7 +44,7 @@ return [
         ],
         'setCookieValidationKey' => [
             'backend/config/main-local.php',
-            'common/config/test-local.php',
+            'common/config/codeception-local.php',
             'frontend/config/main-local.php',
         ],
         'createSymlink' => [
