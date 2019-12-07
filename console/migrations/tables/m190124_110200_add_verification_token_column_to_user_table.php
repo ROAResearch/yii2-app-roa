40afab219e1f24ab5ccec839319c963cb8184b34
Angel Guevara <angeldelcaos@gmail.com>
2019-12-06 19:56:37 -0600

    fix migrations

--- console/migrations/tables/m190124_110200_add_verification_token_column_to_user_table.php
+++ console/migrations/tables/m190124_110200_add_verification_token_column_to_user_table.php
@@ -1,12 +1,16 @@
 <?php
 
-use \yii\db\Migration;
+use yii\db\Migration;
 
 class m190124_110200_add_verification_token_column_to_user_table extends Migration
 {
     public function up()
     {
-        $this->addColumn('{{%user}}', 'verification_token', $this->string()->defaultValue(null));
+        $this->addColumn(
+            '{{%user}}',
+            'verification_token',
+            $this->string()->defaultValue(null)
+        );
     }
 
     public function down()
